37dcf811064357cfbff4b35f8d07c11e4eb1afe3
To-Code-Or-Not-To-Code <rz.codestudio@gmail.com>
2025-10-15 22:17:37 -0700

    Implement the basis for a background component

---
+++ src/components/Background.vue
@@ -0,0 +1,18 @@
+<!-- TODO: MAKE THIS WORK -->
+
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="background" bg-src={{ src }}></div>
+</template>
+
+<style scoped>
+.background {
+    background-image: attr(bg-src);
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+}
+</style>
