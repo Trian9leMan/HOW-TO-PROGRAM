3053008884da00eb29e0bee5f74441e5112e7f63
Canaan <canaan77c0d3r@gmail.com>
2025-10-22 22:14:40 -0700

    gr

--- src/views/Python.vue
+++ src/views/Python.vue
@@ -11,20 +11,20 @@ onMounted(() => {
         const blob2 = document.querySelector('#blob2')
 
         if (!blob1 || !blob2) return
-            KUTE.fromTo(
+
+        const anim = KUTE.fromTo(
             blob1,
             { path: blob1 },
             { path: blob2 },
             {
-            repeat: Infinity,
-            duration: 8000,
-            yoyo: true,
-            easing: 'easingCubicInOut'
+                repeat: Infinity,
+                duration: 8000,
+                yoyo: true,
+                easing: 'easeInOut'
             }
-            )
+        )
 
-
-        anim.start()
+        anim.start()   
     })
 })
 
@@ -45,12 +45,16 @@ onBeforeUnmount(() => {
         <main>
             <h1 class="heading">What is Python?</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, veritatis. Exercitationem dolores veritatis blanditiis optio possimus consequatur, quasi nulla? Soluta molestias cum quaerat unde voluptatibus delectus reprehenderit rem voluptates eaque.
-                Culpa esse sapiente porro sunt quasi aut laborum alias repudiandae quae. Iure quo ipsum sunt, quos minima quasi nemo eos rem tenetur modi sed nostrum quaerat? Est, enim? Velit, reprehenderit.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, veritatis. Exercitationem dolores
+                veritatis blanditiis optio possimus consequatur, quasi nulla? Soluta molestias cum quaerat unde
+                voluptatibus delectus reprehenderit rem voluptates eaque.
+                Culpa esse sapiente porro sunt quasi aut laborum alias repudiandae quae. Iure quo ipsum sunt, quos
+                minima quasi nemo eos rem tenetur modi sed nostrum quaerat? Est, enim? Velit, reprehenderit.
             </p>
             <h1 class="heading">Setting Up</h1>
-            <p class="special">
-                Go to the official Python website to setup python and follow the video below to setup your coding enviroment, so that you can start coding!
+            <p>
+                Go to the official Python website to setup python and follow the video below to setup your coding
+                enviroment, so that you can start coding!
             </p>
             <a href="https://www.python.org/downloads/" class="link">OFFICIAL SITE FOR PYTHON DOWNLOAD</a>
             <video></video>
@@ -60,12 +64,12 @@ onBeforeUnmount(() => {
             <g transform="translate(471.5 301.3)">
                 <path id="blob1"
                     d="M159.3,-66.4C174,-6.5,131.1,57.4,65.5,109.6C0,161.8,-88.2,202.3,-143.6,168.8C-199,135.3,-221.7,28,-192.3,-52.2C-162.9,-132.3,-81.5,-185.1,-4.6,-183.6C72.3,-182.2,144.6,-126.3,159.3,-66.4z"
-                    fill="#1dc6a1" />
+                    fill="#54add6" />
             </g>
             <g transform="translate(445.3 277.9)">
                 <path id="blob2"
                     d="M162,-187.7C200.6,-160.7,216,-100.7,213,-47.9C210,5,188.6,50.7,167.1,104.5C145.6,158.3,124.1,220.1,82.5,241.9C40.8,263.7,-21,245.6,-76,220.5C-131.1,195.4,-179.4,163.5,-196.4,120.1C-213.4,76.8,-199,22.1,-184.4,-27.6C-169.8,-77.4,-155,-122.2,-124.2,-150.6C-93.4,-179,-46.7,-191,7.5,-199.9C61.7,-208.9,123.4,-214.7,162,-187.7z"
-                    fill="#1dc6a1" style="opacity: 0" />
+                    fill="#439cc5" style="opacity: 0" />
             </g>
         </svg>
     </div>
@@ -74,17 +78,17 @@ onBeforeUnmount(() => {
 
 <style scoped>
 .blob-motion {
-  width: clamp(300px, 60vw, 800px);
-  height: auto;
-  margin: 0 auto;
-  display: block;
+    width: clamp(300px, 60vw, 800px);
+    height: auto;
+    margin: 0 auto;
+    display: block;
 }
 
-svg.blob-motion {
+/* svg.blob-motion {
     display: block;
-    margin:auto;
+    margin: auto;
     margin-top: 4rem;
-}
+} */
 
 
 #blob1 {
@@ -96,15 +100,28 @@ svg.blob-motion {
 }
 
 .link {
-  display: block;
-  text-align: center;
-  margin: 1rem auto;
-  color: #1dc6a1;
-  font-weight: 600;
-  text-decoration: none;
-}
- .special {
-    text-align: center;
     display: block;
+    text-align: center;
+    margin: 1rem auto;
+    color: var(--accent-colour);
+    font-weight: 600;
+    text-decoration: none;
+}
+
+svg.blob-motion {
+    width: 75%;
+    height: 75%;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: -1;
+}
+
+@media screen and (max-width: 1024px) {
+    svg.blob-motion {
+        left: 50%;
+        margin-left: -512px;
+    }
 }
 </style>
